ab1db1791c5c02d238737ab6f9fe663f324ee5ff
John Hayes <johnnyhayes7@gmail.com>
2019-01-27 02:50:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.count == 0 
+    puts "The line is currently empty."
+  else
+    new_array = []
+    katz_deli.each_with_index do |name, index|
+      listnumber_name = "#{index + 1}. #{name}"
+      new_array << listnumber_name
+      new_array.join(" ")
+    end
+    puts "The line is currently: #{new_array}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
+end
